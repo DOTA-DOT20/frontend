bb218c2090edccc1297637adac584c7b9578235e
theworld <aol56794@gmail.com>
2023-12-17 23:52:44 +1300

    token table

--- src/app/token/page.tsx
+++ src/app/token/page.tsx
@@ -1,8 +1,45 @@
-export default function Home() {
+import { getTokens } from "@/app/utils/get-tokens"
+import  styles from "./index.module.css";
+
+export default async function Home() {
+    const tokens = await getTokens(1, 1, "none")
     return (
         <main className="min-h-screen p-24">
-            <h2>token list</h2>
-
+            <div className={`w-full border px-5 py-4 ${styles.tableContainer}`}>
+                <table className="table-auto w-full">
+                    <thead className={`opacity-60 h-10 ${styles.tableHeader}`}>
+                        <tr>
+                            <th className="rounded-l-3xl">#</th>
+                            <th>Name</th>
+                            <th>Progress</th>
+                            <th>Total Supply</th>
+                            <th>Minted %</th>
+                            <th>Holders</th>
+                            <th className="rounded-r-3xl">Deploy Block</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {tokens.map((token, index) => {
+                            const progressDoneWidth = token.progress / 100 * 200;
+                            return (
+                                <tr key={token.name} className={`h-24 border-b ${styles.tableRow}`}>
+                                    <td className="text-center">{index + 1}</td>
+                                    <td className="text-center">{token.name}</td>
+                                    <td className="h-24 flex justify-center items-center">
+                                        <div className={`h-4 rounded-xl ${styles.progressAll}`} style={{width: 200}}>
+                                            <div className={`h-4 rounded-xl ${styles.progressDone}`} style={{width: progressDoneWidth}}></div>
+                                        </div>
+                                    </td>
+                                    <td className="text-center">{token.totalSupply}</td>
+                                    <td className="text-center">{token.progress}%</td>
+                                    <td className="text-center">{token.holder}</td>
+                                    <td className="text-center">{token.deployedBlock}</td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
+            </div>
         </main>
     )
 }
---
+++ src/app/utils/get-tokens.ts
@@ -0,0 +1,11 @@
+// @ts-ignore
+import { cache } from 'react'
+
+export const getTokens = cache(async (page: number, pageSize: number, orderBy: string) => {
+  const fakeData = [
+    {name: "dota", progress: 50, totalSupply: "21,000,000", holder: "1,000,000", deployedBlock: "566666"},
+    {name: "radi", progress: 60, totalSupply: "1,000,000", holder: "100,000", deployedBlock: "566666"},
+    {name: "lolo", progress: 100, totalSupply: "1,000,000", holder: "200,000", deployedBlock: "566666"},
+  ]
+  return fakeData
+})
