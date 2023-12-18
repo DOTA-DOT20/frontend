0cfc67a3b293fa2fae169735faa6203406526277
Tiyo <tiyosheng@gmail.com>
2023-12-18 19:20:04 +0800

    [feat] deploy

--- src/app/inscribe/page.tsx
+++ src/app/inscribe/page.tsx
@@ -8,19 +8,51 @@ import {InjectedAccountWithMeta} from "@polkadot/extension-inject/types";
 import {accountState} from "@/stores/account";
 import {isWeb3Injected, web3FromAddress} from "@polkadot/extension-dapp";
 import {useConnectWallet} from "@/hooks/usePolkadot";
-import {async} from "rxjs";
 
 
 export default function Home() {
     const [checkedType, setChecked] = useState('mint')
 
     const [tick, setTick] = useState("DOTA");
-    const [amount, setAmount] = useState("1000");
+    const [amount, setAmount] = useState("500000");
 
     const { connect, getApi, getInjectedAccount, selectedAccount } = useConnectWallet()
 
     const handleChanged = (event: ChangeEvent<any>) => {
+        setTick('')
+        setAmount('')
         setChecked(event.target.value)
+    }
+
+    const transfer = async (info: any) => {
+        const api = await getApi()
+        const injector = await getInjectedAccount()
+        if (injector) {
+            api.tx.utility.batchAll([
+            api.tx.balances.transferKeepAlive(selectedAccount.address, 1 * 1e12),
+            api.tx.system.remark(JSON.stringify(info)),
+            ]).signAndSend(selectedAccount.address, { signer: injector.signer }, (result: any) => {
+            if (result.status.isInBlock) {
+            } else if (result.status.isFinalized) {
+                let blockNumber = result.blockNumber.toNumber()
+                console.log('success! blockNumber:', blockNumber)
+            }
+            });
+        }
+    }
+
+    const handleDeploy = async () => {
+        let info = {
+            p: "dot-20",
+            op: "deploy",
+            tick,
+            amount
+        }
+        if(selectedAccount?.address) {
+            transfer(info)
+        } else {
+            await connect()
+        }
     }
 
 
@@ -31,21 +63,7 @@ export default function Home() {
             tick
         }
         if(selectedAccount?.address) {
-            const api = await getApi()
-            const injector = await getInjectedAccount()
-            if (injector) {
-              api.tx.utility.batchAll([
-                api.tx.balances.transferKeepAlive(selectedAccount.address, 1 * 1e12),
-                api.tx.system.remark(JSON.stringify(info)),
-              ]).signAndSend(selectedAccount.address, { signer: injector.signer }, (result: any) => {
-                if (result.status.isInBlock) {
-                } else if (result.status.isFinalized) {
-                  let blockNumber = result.blockNumber.toNumber()
-                  console.log('success! blockNumber:', blockNumber)
-                }
-              });
-            }
-
+            transfer(info)
         } else {
             await connect()
         }
@@ -107,6 +125,48 @@ export default function Home() {
                                 >
                                     CONNECT WALLET
                                 </Button>
+                        }
+                  </div>
+                </> }
+                {checkedType === 'deploy' && <>
+                  <div className={styles.contentBody}>
+                    <div className={styles.form}>
+                      <div className={styles.formItem}>
+                        <label htmlFor="name">Ticket</label>
+                        <Input
+                          placeholder="4 characters like 'DOTA'..."
+                          value={tick}
+                          onValueChange={setTick}
+                        />
+                      </div>
+                      <div className={styles.formItem}>
+                        <label htmlFor="name">Amount</label>
+                        <Input
+                            type="number"
+                            min={1}
+                            placeholder="Amount"
+                            value={amount}
+                            onValueChange={setAmount}
+                        />
+                        </div>
+                    </div>
+                  </div>
+                  <div className={styles.contentFooter}>
+                      {
+                        selectedAccount?.address ?
+                            <Button className="btn btn-large bg-pink-500 hover:bg-sky-700 block flex-1 color-white"
+                                size="lg"
+                                onClick={handleDeploy}
+                            >
+                                DEPLOY
+                            </Button>
+                            :
+                            <Button className="btn btn-large bg-pink-500 hover:bg-sky-700 block flex-1 color-white"
+                                size="lg"
+                                onClick={handleConnet}
+                            >
+                                CONNECT WALLET
+                            </Button>
                         }
                   </div>
                 </> }
