6d8e88b5f91d82f142f0f17c3fd878fcd1e6f67f
momodaka <463435681@qq.com>
2023-12-25 16:11:24 +0800

    feat: footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,32 +4,44 @@ import Image from "next/image";
 import twitterIcon from '@/icons/twitter.svg';
 import docIcon  from '@/icons/book.svg';
 import githubIcon  from '@/icons/github.svg';
+import copyrightIcon  from './copyright.svg';
 import styles from './index.module.css';
+
 export default function Footer() {
     return <div className={styles.footer}>
-        <a  href="https://twitter.com/dot20_dota" target="_blank">
+        <div className={styles.footerNav}>
+            <a  href="https://twitter.com/dot20_dota" target="_blank">
+                <Image
+                    src={twitterIcon}
+                    width={32}
+                    height={32}
+                    alt="twitter"
+                />
+            </a>
+            <a href="https://github.com/DOTA-RC20" target="_blank">
+                <Image
+                    src={githubIcon}
+                    width={32}
+                    height={32}
+                    alt="twitter"
+                />
+            </a>
+            <a>
+                <Image
+                    src={docIcon}
+                    width={32}
+                    height={32}
+                    alt="doc"
+                />
+            </a>
+        </div>
+        <div className={styles.copyRight}>
             <Image
-                src={twitterIcon}
-                width={32}
-                height={32}
-                alt="twitter"
+                src={copyrightIcon}
+                width={385}
+                height={50}
+                alt="Secured by PolkaDot"
             />
-        </a>
-        <a href="https://github.com/DOTA-RC20" target="_blank">
-            <Image
-                src={githubIcon}
-                width={32}
-                height={32}
-                alt="twitter"
-            />
-        </a>
-        <a>
-            <Image
-                src={docIcon}
-                width={32}
-                height={32}
-                alt="doc"
-            />
-        </a>
+        </div>
     </div>
 }
